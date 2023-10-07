c7ec83f874a8980b77f0d9356c661bb590867483
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-07 22:23:40 +0400

    Add files via upload

---
+++ Task 2.8/index.js
@@ -0,0 +1,16 @@
+
+let x="";
+
+for(let i=0;i<5;i++){
+    x+='X'
+    console.log(x)
+}
+
+
+for(let i=6;i>0;i--){
+    let y="";
+    for(let j=0;j<i;j++){
+        y=y+'X'
+    }
+    console.log(y)
+}
